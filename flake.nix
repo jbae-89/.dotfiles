724a15ac88c96fe46f58e0f1dd7134727c2da73e
jbae-89 <joshua.e.bailey1@gmail.com>
2026-02-27 11:12:28 -0500

    initial config

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+
+description = "Default_Flake";
+
+inputs = {
+
+#	nixpkgs = {
+#	url = "github:/NixOS/nixpkgs/nixos-25.11";
+#	};
+
+	nixpkgs.url = "github:/NixOS/nixpkgs/nixos-25.11";
+
+#	nixpkgs.url = "nixpkgs/nixos-25.11";
+
+
+home-manager = {
+      url = "github:nix-community/home-manager/release-25.11";
+      # This line ensures home-manager uses the same nixpkgs version as your system
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+};
+
+
+
+outputs = {self, nixpkgs, home-manager, ...}:
+
+	let 
+		lib = nixpkgs.lib;
+	in { 
+
+	nixosConfigurations = {	
+	nixos = lib.nixosSystem {
+		
+		system = "x86_64-linux";
+		modules = [
+					./configuration.nix
+
+					# Import the Home Manager NixOS module
+					      home-manager.nixosModules.home-manager
+					      {
+					        home-manager.useGlobalPkgs = true;
+					        home-manager.useUserPackages = true;
+					        home-manager.users.josh = import ./home.nix; # Point to your home.nix
+					      }
+
+					
+				  ];		
+		
+		};
+
+	};
+
+};	
+
+}
